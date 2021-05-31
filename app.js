e39e6adb1d2d7ccc68d4ca922efb71f0ae0600e1
DimitraK92 <79407886+DimitraK92@users.noreply.github.com>
2021-06-01 00:12:29 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    var player = '<div id="player"></div>';
+    $("#map").append(player);
+
+    $(document).keydown(function (e) {
+        var position = $("#player").position();
+        switch (e.keyCode) {
+            case 37:    //Left
+                $("#player").css('left',position.left - 50 + 'px');
+                break;
+            case 38:    //Up
+                $("#player").css('top',position.top - 50 + 'px'); 
+                break;
+            case 39:    //Right
+                $("#player").css('left', position.left + 50 + 'px'); 
+                break;
+            case 40:    //Down
+                $("#player").css('top', position.top + 50 + 'px'); 
+                break;
+        }
+    })
+    window.addEventListener("keydown", function (e) {
+        if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
+            e.preventDefault();
+        }
+    }, false);
+});
