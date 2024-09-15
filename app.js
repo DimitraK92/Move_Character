9bac9d69d1d85c1fa98550c10aaeed892f89fcc7
dkamni <dimitra.kamni@gmail.com>
2024-09-15 23:03:08 +0300

    small refactoring/reformatting

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 ﻿$(function () {
-    var player = '<div id="player"></div>';
+    const player = '<div id="player"></div>';
     $("#map").append(player);
 
     $(document).keydown(function (e) {
-        var position = $("#player").position();
-        switch (e.keyCode) {
-            case 37:    //Left
-                $("#player").css('left',position.left - 50 + 'px');
-                break;
-            case 38:    //Up
-                $("#player").css('top',position.top - 50 + 'px'); 
-                break;
-            case 39:    //Right
-                $("#player").css('left', position.left + 50 + 'px'); 
-                break;
-            case 40:    //Down
-                $("#player").css('top', position.top + 50 + 'px'); 
-                break;
+        let position = $('#player').position();
+        if (e.keyCode === 37) {//Left
+            $("#player").css('left', position.left - 50 + 'px');
+        } else if (e.keyCode === 38) {//Up
+            $("#player").css('top', position.top - 50 + 'px');
+        } else if (e.keyCode === 39) {//Right
+            $("#player").css('left', position.left + 50 + 'px');
+        } else if (e.keyCode === 40) {//Down
+            $("#player").css('top', position.top + 50 + 'px');
         }
     })
     window.addEventListener("keydown", function (e) {
